104946278ebee4907ef55f755e316fbd26d0557e
Le Dai <dl123@mail.ustc.edu.cn>
2021-11-03 11:20:25 +0800

    feat: support vue

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log('hello, world!')
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+// plugins
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue(),
+  ],
+})
