62804006cc869a73619d91fed216e9301d5b9922
Le Dai <dl123@mail.ustc.edu.cn>
2021-11-05 11:00:26 +0800

    feat: support ssg for better seo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
 // plugins
-import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { createHead } from '@vueuse/head'
 // style
 import 'virtual:windi.css'
 import 'prism-theme-vars/base.css'
 import 'prism-theme-vars/themes/vitesse-light.css'
 
-const app = createApp(App)
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: setupLayouts(routes),
-})
-app.use(router)
-
-const head = createHead()
-app.use(head)
-
-app.mount('#app')
+export const createApp = ViteSSG(App, { routes: setupLayouts(routes) })
