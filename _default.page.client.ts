eba119767973afc25f20b1f4ec0506dc2efa6124
Yu Yin <yxonic@gmail.com>
2021-11-24 21:59:22 +0800

    fix: server routing global default

---
+++ _default.page.client.ts
@@ -0,0 +1,8 @@
+// load server pages on navigation by default
+import { hydrate } from 'vpage/client'
+hydrate()
+
+// to setup client router globally, replace the above with:
+
+// import { setupClientRouter } from 'vpage/router'
+// setupClientRouter()
