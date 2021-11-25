a03fbbadf05b31266fd97c93b65e683ea64aa549
lem0nle <lem0nle@users.noreply.github.com>
2021-11-25 16:48:34 +0000

    deploy: 9c6fd5a22efdd8d0f618310ef6f6e13df6c4e273

---
+++ assets/_default.page.client.ts.e64fef1b.js
@@ -0,0 +1,1 @@
+import{g as p}from"./vendor.170e44b4.js";import{c as o}from"./app.9b22a269.js";async function c(){const a=await p(),{app:t}=await o(a);t.mount("#app")}c();
---
+++ assets/examples/project/_default.page.client.ts.f4b0fb21.js
@@ -0,0 +1,1 @@
+import{s as o}from"../../router.6548e145.js";import"../../vendor.170e44b4.js";import"../../app.9b22a269.js";o({onTransitionStart(){console.log("start transition")},onTransitionEnd(){console.log("end transition")}});
