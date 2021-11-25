e9a95f60dfa933989723f62d2b105fc0efd170cf
Yu Yin <yxonic@gmail.com>
2021-11-26 01:02:01 +0800

    fix: use linkedom instead of jsdom

--- _vpage/lib/server.ts
+++ _vpage/lib/server.ts
@@ -1,18 +1,18 @@
 import { renderToNodeStream } from '@vue/server-renderer'
 import { escapeInject } from 'vite-plugin-ssr'
-import { JSDOM } from 'jsdom'
+import { parseHTML } from 'linkedom'
 import { PageContext } from './context'
 import { createApp } from '../renderer/app'
 
 export function setupServerRenderer(html: string) {
-  const jsdom = new JSDOM(html)
+  const { document } = parseHTML(html)
 
   async function render(ctx: PageContext) {
     const { app, head } = await createApp(ctx)
 
     const stream = renderToNodeStream(app)
-    head.updateDOM(jsdom.window.document)
-    const html = jsdom.serialize()
+    head.updateDOM(document)
+    const html = document.toString()
 
     const stringArray = html.split('<!-- vpage-content -->')
     Object.assign(stringArray, { raw: stringArray })
