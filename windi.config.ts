6392c98d1532a73a299fa4c56e092a8ec4426436
Yu Yin <yxonic@gmail.com>
2021-12-01 16:25:32 +0800

    fix: windicss preflight styles

--- windi.config.ts
+++ windi.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite-plugin-windicss'
 import typography from 'windicss/plugin/typography'
 
 export default defineConfig({
+  preflight: {
+    safelist: 'h1 h2 h3 p ul ol li img hr',
+  },
   extract: {
-    include: ['_vpage/**', 'docs/**', 'examples/**', 'src/**', 'index.html'],
+    include: [
+      '{_vpage,docs,examples,src}/**/*.{jsx,tsx,html,vue,md}',
+      'index.html',
+    ],
   },
   fontFamily: {
     sans: '"Noto Sans", "Noto Sans SC", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
