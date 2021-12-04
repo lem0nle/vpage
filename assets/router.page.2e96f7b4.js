ea775ad3fb80037b469c9569c3653727c4242c30
lem0nle <lem0nle@users.noreply.github.com>
2021-12-04 10:28:51 +0000

    deploy: d5415a17acc6b6dc924f7070f8bbb9fa5a0b8533

--- assets/router.page.2e96f7b4.js
+++
@@ -1,1 +0,0 @@
-import{o,c as e,l as r}from"./vendor.2fe9af50.js";const s={class:"markdown-body"},a=r("h1",{id:"router",tabindex:"-1"},"Router",-1),c=[a],u={title:"Router - VPage Docs",layout:"book"},f={setup(n,{expose:t}){return t({frontmatter:{title:"Router - VPage Docs",layout:"book"}}),(d,i)=>(o(),e("div",s,c))}};export{f as default,u as frontmatter};
