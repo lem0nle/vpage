292cfbd28d2eab057add3d5a9fe82a53002495a5
Yu Yin <yxonic@gmail.com>
2021-12-06 11:46:43 +0800

    feat: use vmark

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import { defineConfig } from 'vite'
 // plugins
 import SSR from 'vite-plugin-ssr/plugin'
 import Vue from '@vitejs/plugin-vue'
-import Markdown from 'vite-plugin-md'
-import Prism from 'markdown-it-prism'
-import LinkAttributes from 'markdown-it-link-attributes'
-import ReplaceLink from 'markdown-it-replace-link'
-import Anchor from 'markdown-it-anchor'
+import VMark from '@yxonic/vmark/vite'
 import WindiCSS from 'vite-plugin-windicss'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
@@ -25,32 +21,8 @@ export default defineConfig({
   },
   plugins: [
     SSR(),
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    Markdown({
-      markdownItOptions: {
-        replaceLink(link: string) {
-          if (link.startsWith('/')) {
-            return base + link.slice(1)
-          }
-          return link
-        },
-      } as never,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism)
-        md.use(LinkAttributes, {
-          pattern: /^https?:\/\//,
-          attrs: {
-            target: '_blank',
-            rel: 'noopener',
-          },
-        })
-        md.use(ReplaceLink)
-        md.use(Anchor)
-      },
-    }),
+    Vue(),
+    VMark(),
     WindiCSS(),
     Components({
       extensions: ['vue', 'md'],
